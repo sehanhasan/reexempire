19de519a9fc31c4476baf2611be54f3bed7eeff4
sehanhasan <sehan@izecon.com>
2025-08-14 02:18:40 +0500

    mt

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -190,7 +190,7 @@ export default function Customers() {
         title="Customers" 
       />
       
-      <div className="mt-8">
+      <div>
         <DataTable 
           columns={columns} 
           data={customers} 
